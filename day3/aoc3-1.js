ba125539ce2ff1db342dd9f02b51bd9b8740a305
FallDown <8807171+FallDownTheSystem@users.noreply.github.com>
2018-12-04 23:36:11 +0200

    Day 3 fix

--- day3/aoc3-1.js
+++ day3/aoc3-1.js
@@ -7,14 +7,13 @@ const overlap = data => {
 		.split('\n')
 		.forEach(row => {
 			let match = row
-				.match(/#\d+\s@\s(\d+),(\d+):\s(\d+)x(\d+)/)
+				.match(/#(\d+)\s@\s(\d+),(\d+):\s(\d+)x(\d+)/)
 				.slice(1, 6)
 				.map(Number);
 			const xOffset = match[1];
 			const yOffset = match[2];
 			const width = match[3];
 			const height = match[4];
-			t0 = performance.now();
 
 			for (let i = 0; i < width; i++) {
 				let x = xOffset + i;
@@ -24,8 +23,6 @@ const overlap = data => {
 				}
 			}
 		});
-	var t1 = performance.now();
-	console.log(t1 - t0);
 	return Object.values(area).filter(x => x > 1).length;
 };
 
