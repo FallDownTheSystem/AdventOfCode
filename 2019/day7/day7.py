e61393ae23dd086f0689adc7f1ce9e02892a1619
FallDown <8807171+FallDownTheSystem@users.noreply.github.com>
2019-12-07 22:35:11 +0200

    Day 7 part 2

--- 2019/day7/day7.py
+++ 2019/day7/day7.py
@@ -1,5 +1,6 @@
 import itertools
 
+# Operation Codes
 ADD = 1
 MULTIPLY = 2
 INPUT = 3
@@ -9,6 +10,10 @@ JUMP_IF_FALSE = 6
 LESS_THAN = 7
 EQUALS = 8
 HALT = 99
+
+# Return Codes
+HALTED = -1
+WAITING_FOR_INPUT = -2
 
 
 class Instruction:
@@ -25,6 +30,7 @@ class CPU:
         self.ip = 0
         self.input_device = inputs
         self.output_device = None
+        self.return_code = None
 
     def viewMemory(self):
         memoryAsStr = ""
@@ -66,8 +72,11 @@ class CPU:
             self.memory[address] = val_a * val_b
             return
         elif instr.opcode == INPUT:
+            if len(self.input_device) == 0:
+                self.ip -= 1
+                return WAITING_FOR_INPUT
             address = self.fetch()
-            self.memory[address] = self.input_device.pop()
+            self.memory[address] = self.input_device.pop(0)
             return
         elif instr.opcode == OUTPUT:
             a = self.fetch()
@@ -123,33 +132,64 @@ class CPU:
                 self.memory[address] = 0
             return
         elif instr.opcode == HALT:
-            return -1
+            return HALTED
 
     def step(self):
         instruction = self.fetch()
-        return self.execute(instruction)
+        self.return_code = self.execute(instruction)
+        return self.return_code
 
 
-def compute(int_code: str, inputs: list):
-    memory = [int(x) for x in int_code.split(",")]
-    cpu = CPU(memory, inputs)
+def compute(cpu: CPU):
     running = True
     while running:
-        running = cpu.step() != -1
+        ret_code = cpu.step()
+        running = ret_code is None
     return cpu
 
 
-def max_thruster_signal(puzzle_input):
+def max_thruster_signal(memory):
     phaser_sequences = list(itertools.permutations([0, 1, 2, 3, 4]))
     outputs = []
     for sequence in phaser_sequences:
         output = 0
         for phaser_input in sequence:
-            output = compute(puzzle_input, [output, phaser_input]).output_device
+            cpu = CPU(memory[:], [phaser_input, output])
+            output = compute(cpu).output_device
         outputs.append(output)
     return max(outputs)
 
 
 print("==================== Part 1 ====================")
 puzzle_input = open("2019/day7/input.txt", "r").read().rstrip()
-print("Max thruster signal:", max_thruster_signal(puzzle_input))
+memory = [int(x) for x in puzzle_input.split(",")]
+print("Max thruster signal:", max_thruster_signal(memory))
+
+
+def feedback_loop(memory):
+    phaser_sequences = list(itertools.permutations([5, 6, 7, 8, 9]))
+    thruster_outputs = []
+    for sequence in phaser_sequences:
+        phasers = []
+        for phaser_input in sequence:
+            cpu = CPU(memory[:], [phaser_input])
+            phasers.append(cpu)
+        phasers[0].input_device.append(0)
+        index = 0
+        while True:
+            curr_index = index % len(phasers)
+            next_index = (index + 1) % len(phasers)
+            cpu = phasers[curr_index]
+            cpu = compute(cpu)
+            index += 1
+            if cpu.return_code == WAITING_FOR_INPUT or (cpu.return_code == HALTED and curr_index != len(phasers) - 1):
+                phasers[next_index].input_device.append(cpu.output_device)
+            elif (curr_index == len(phasers) - 1) and cpu.return_code == HALTED:
+                thruster_outputs.append(cpu.output_device)
+                break
+
+    return max(thruster_outputs)
+
+
+print("==================== Part 2 ====================")
+print("Max thruster signal from feedback loop:", feedback_loop(memory))
