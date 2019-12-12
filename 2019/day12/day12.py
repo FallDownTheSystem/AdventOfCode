eaa0819c31b0e8a7deb06bf0ca17e7ff4b32f40c
FallDown <N/A>
2019-12-12 15:19:14 +0200

    Day 12 WIP

--- 2019/day12/day12.py
+++ 2019/day12/day12.py
@@ -33,4 +33,21 @@ if __name__ == '__main__':
 
     print("==================== Part 1 ====================")
     for moon in moons:
-        print(moon.position)
+        for target in moons:
+            if moon == target:
+                continue
+            if moon.pos_x < target.pos_x:
+                moon.vel_x += 1
+            elif moon.pos_x > target.pos_x:
+                moon.vel_x -= 1
+
+            if moon.pos_y < target.pos_y:
+                moon.vel_y += 1
+            elif moon.pos_y > target.pos_y:
+                moon.vel_y -= 1
+
+            if moon.pos_z < target.pos_z:
+                moon.vel_z += 1
+            elif moon.pos_z > target.pos_z:
+                moon.vel_z -= 1
+        print(moon.velocity)
