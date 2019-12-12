2319480e8cafb0e7051c6f3aee551819282b0805
FallDown <8807171+FallDownTheSystem@users.noreply.github.com>
2019-12-12 19:47:59 +0200

    Day 12

--- 2019/day12/day12.py
+++ 2019/day12/day12.py
@@ -1,3 +1,6 @@
+from math import gcd
+
+
 class Moon():
     def __init__(self, pos, vel=(0, 0, 0)):
         self.pos_x, self.pos_y, self.pos_z = pos
@@ -7,17 +10,101 @@ class Moon():
     def position(self):
         return (self.pos_x, self.pos_y, self.pos_z)
 
-    @position.setter
-    def position(self, pos):
-        self.pos_x, self.pos_y, self.pos_z = pos
-
     @property
     def velocity(self):
         return (self.vel_x, self.vel_y, self.vel_z)
 
-    @velocity.setter
-    def velocity(self, vel):
-        self.vel_x, self.vel_y, self.vel_z = vel
+    def update_position(self):
+        self.pos_x += self.vel_x
+        self.pos_y += self.vel_y
+        self.pos_z += self.vel_z
+
+    def energy(self):
+        potential_energy = abs(self.pos_x) + abs(self.pos_y) + abs(self.pos_z)
+        kinetic_energy = abs(self.vel_x) + abs(self.vel_y) + abs(self.vel_z)
+        return potential_energy * kinetic_energy
+
+    def plane(self, index):
+        return str(self.position[index]) + str(self.velocity[index])
+
+    def apply_gravity(self, target):
+        if self.pos_x < target.pos_x:
+            self.vel_x += 1
+        elif self.pos_x > target.pos_x:
+            self.vel_x -= 1
+
+        if self.pos_y < target.pos_y:
+            self.vel_y += 1
+        elif self.pos_y > target.pos_y:
+            self.vel_y -= 1
+
+        if self.pos_z < target.pos_z:
+            self.vel_z += 1
+        elif self.pos_z > target.pos_z:
+            self.vel_z -= 1
+
+
+def gravitate(moons):
+    for moon in moons:
+        for target in moons:
+            if moon == target:
+                continue
+            moon.apply_gravity(target)
+
+
+def simulate(moons):
+    for i in range(1000):
+        gravitate(moons)
+        for moon in moons:
+            moon.update_position()
+    return moons
+
+
+def cyclic(moons):
+    steps = []
+    step = 0
+    seenX = set()
+    seenY = set()
+    seenZ = set()
+    foundX = False
+    foundY = False
+    foundZ = False
+    while True:
+        x = y = z = ''
+        gravitate(moons)
+        for moon in moons:
+            moon.update_position()
+            x += moon.plane(0)
+            y += moon.plane(1)
+            z += moon.plane(2)
+        # Check if the plane has been in this position before
+        if not foundX:
+            if x in seenX:
+                steps.append(step)
+                foundX = True
+            else:
+                seenX.add(x)
+        if not foundY:
+            if y in seenY:
+                steps.append(step)
+                foundY = True
+            else:
+                seenY.add(y)
+        if not foundZ:
+            if z in seenZ:
+                steps.append(step)
+                foundZ = True
+            else:
+                seenZ.add(z)
+
+        if foundX and foundY and foundZ:
+            break
+        step += 1
+    return steps
+
+
+def lcm(a, b):
+    return abs(a * b) // gcd(a, b)
 
 
 if __name__ == '__main__':
@@ -32,22 +119,12 @@ if __name__ == '__main__':
         moons.append(Moon(tuple(coords)))
 
     print("==================== Part 1 ====================")
-    for moon in moons:
-        for target in moons:
-            if moon == target:
-                continue
-            if moon.pos_x < target.pos_x:
-                moon.vel_x += 1
-            elif moon.pos_x > target.pos_x:
-                moon.vel_x -= 1
+    part1 = simulate(moons[:])
+    total_energy = 0
+    for moon in part1:
+        total_energy += moon.energy()
+    print(total_energy)
 
-            if moon.pos_y < target.pos_y:
-                moon.vel_y += 1
-            elif moon.pos_y > target.pos_y:
-                moon.vel_y -= 1
-
-            if moon.pos_z < target.pos_z:
-                moon.vel_z += 1
-            elif moon.pos_z > target.pos_z:
-                moon.vel_z -= 1
-        print(moon.velocity)
+    print("==================== Part 2 ====================")
+    steps = cyclic(moons[:])
+    print(lcm(steps[0], lcm(steps[1], steps[2])))
